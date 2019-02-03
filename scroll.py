f96918eefe6f07ebca0f8c041f62f844dacdc2f0
Andrei <47288274+dandred@users.noreply.github.com>
2019-02-03 02:43:14 -0500

    Update scroll.py

--- scroll.py
+++ scroll.py
@@ -5,7 +5,7 @@ from time import sleep
 URL = 'http://scrollmagic.io/examples/advanced/infinite_scrolling.html'
 
 # Change the n number to cover the entire lenght of the page
-# Try different values of sleep_time_s, depending on how fast the page is loading
+# Try different values of sleep_time_s, depending on how fast the sections are loading
 def scroll(browser, n=100, sleep_time_s=4 ):
     for _ in range(n):
         browser.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
